a7573a1500845b0a05d6f6e251b5902fb301bd40
tuyo1009 <tuyo1009@gmail.com>
2015-08-18 23:37:55 +0900

    第９章ユーザー更新・表示・削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :edit, :update, :destroy]
+	before_action :correct_user,   only: [:edit, :update]
+	before_action :admin_uesr, 	   only: :destroy
+	
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed."
+		redirect_to users_url
+	end
+
+	def index
+		# @users = User.all
+		# @users = User.paginate(page: params[:page])
+		@title = "All users"
+    	@users = User.paginate(:page => params[:page])
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -18,11 +35,45 @@ class UsersController < ApplicationController
 		end
 	end
 
-	private
-	def user_params
-		params.require(:user).permit(:name, :email, :password,
-			:password_confirmation)
-
+	def edit
+		# @user = User.find(params[:id])
 	end
 
+	def update
+		# @user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			#更新に成功した場合
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :email, :password,
+				:password_confirmation)
+
+		end
+
+		#before actions
+
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_url, notice: "Please sign in."
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_path) unless current_user.admin?
+		end
 end
